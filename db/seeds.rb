0c1b2ced07fe7e96e9522102910eba45068c8faa
Aysha Areej <aysha@devbox.co>
2022-10-05 14:38:01 +0500

    databse files revert

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 developer = User.create(name: "Developer", user_name: "developer", email: "developer@gmail.com", password: "devbox@123", roles_mask: 1)
-uper_admin = User.create(name: "F99Admin", user_name: "f_99_super_admin", email: "f_99_super_admin@gmail.com", password: "devbox@123", roles_mask: 2, created_by_id: developer.id, company_type: "batha")
+super_admin = User.create(name: "F99Admin", user_name: "f_99_super_admin", email: "f_99_super_admin@gmail.com", password: "devbox@123", roles_mask: 2, created_by_id: developer.id, company_type: "f99")
 User.create(name: "F99Admin", user_name: "f_99_admin", email: "f_99_admin@gmail.com", password: "devbox@123", roles_mask: 4, created_by_id: super_admin.id)
-super_admin = User.create(name: "POSAdmin", user_name: "pos_super_admin", email: "pos_super_admin@gmail.com", password: "devbox@123", roles_mask: 2, created_by_id: developer.id, company_type: "pos")
-User.create(name: "F99Admin", user_name: "pos_admin", email: "pos_admin@gmail.com", password: "devbox@123", roles_mask: 4, created_by_id: super_admin.id)
+
+
 Country.create(title: "Pakistan")
 City.create(title: "Lahore")
 ItemType.create(title: "default",code: "default")
