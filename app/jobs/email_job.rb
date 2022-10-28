15f5f28e3dafb1228ced94053e4ef45b4fec0adb
Hassan Mehmood <hassanmehmood.psr@gmail.com>
2022-10-28 17:14:59 +0500

    modals and T-001 fixes

--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -1,9 +1,9 @@
 class EmailJob < ActiveJob::Base
   queue_as :default
 
-  def perform(data, path, reciever, email_choice, current_user)
+  def perform(data, path, reciever, email_choice, subject, body, current_user, file_name)
     ActiveRecord::Base.establish_connection "#{Rails.env}_#{current_user.company_type}".to_sym
-    if (email_choice.eql? 'CSV') ||( email_choice.eql? 'Both')
+    if (email_choice.eql? 'CSV') || (email_choice.eql? 'Both')
       headers = data&.first&.keys
       @csv_file = CSV.generate(headers: true) do |csv|
         csv << headers
@@ -14,7 +14,6 @@ class EmailJob < ActiveJob::Base
     end
     if (email_choice.eql? 'PDF') || (email_choice.eql? 'Both')
       @pos_setting = PosSetting.first
-      subject = "#{@pos_setting.display_name} - Detail"
       @pdf_path = WickedPdf.new
                            .pdf_from_string(
                              ApplicationController.new.render_to_string(
@@ -24,18 +23,18 @@ class EmailJob < ActiveJob::Base
                                  margin_top: @pos_setting&.pdf_margin_top.to_f,
                                  margin_right: @pos_setting&.pdf_margin_right.to_f,
                                  margin_bottom: @pos_setting&.pdf_margin_bottom.to_f,
-                                 margin_left: @pos_setting&.pdf_margin_left.to_f,
+                                 margin_left: @pos_setting&.pdf_margin_left.to_f
                                },
                                footer: {
                                  right: '[page] of [topage]'
                                }
                              )
-                            )
+                           )
     end
     email = reciever.present? ? reciever : current_user.email
-    csv = [[@csv_file, 'name']]
-    pdf = [[@pdf_path, 'name']]
+    csv = [[@csv_file, file_name]]
+    pdf = [[@pdf_path, file_name]]
 
-    ReportMailer.send_csv_pdf_email(csv, pdf, subject, email, '').deliver
+    ReportMailer.send_csv_pdf_email(csv, pdf, subject, email, body).deliver
   end
 end
