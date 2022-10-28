b3a16a9bee26065177c0a71f52776f229fc4d175
Aysha Areej <aysha@devbox.co>
2022-10-28 18:43:28 +0500

    checkbox-index-page

--- app/controllers/bulk_imports_controller.rb
+++ app/controllers/bulk_imports_controller.rb
@@ -25,4 +25,15 @@ class BulkImportsController < ApplicationController
     end
     redirect_to request.referrer
   end
+
+  def bulk_delete_data
+    data = params[:table_name].constantize.where(id: params[:ids])
+    if data.present?
+      data.destroy_all
+      respond_to do |format|
+      format.html { redirect_to cities_url, notice: 'City was successfully destroyed.' }
+      end
+    end
+    
+  end
 end
