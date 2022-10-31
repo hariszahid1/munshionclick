d9221442245290d2b7f31b0262d979f1eb26f948
Aysha Areej <aysha@devbox.co>
2022-10-31 11:57:17 +0500

    countries-checkbox

--- app/assets/javascripts/chechbox-index.js
+++ app/assets/javascripts/chechbox-index.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
-
   $('.select-all-checkbox').on("click", function () {
     var cbxs = $('input[name="object_ids[]"]');
     cbxs.prop("checked", !cbxs.prop("checked"));
   });
-
+  
   $('.delete-selected').on('click', function () {
     var object_ids = $('input[name="object_ids[]"]:checked')
     var ids = object_ids.map(function (i, e) { return e.value }).toArray();
     var table_name = $(this).val()
-    let text = "Are u sure u want to delete this item?";
+    var text_data = "Are u sure u want to delete this item?";
     if (object_ids.length > 0){
-      if (confirm(text) == true) {
+      if (confirm(text_data) == true) {
         $.ajax({
           url: '/bulk_delete_data',
           type: 'POST',
