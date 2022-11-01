46656500f74611e35b81c96c0e5a68dc505bc0b5
Aysha Areej <aysha@devbox.co>
2022-11-01 11:04:40 +0500

    item-type

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -1,25 +1,19 @@
 class ItemTypesController < ApplicationController
+  include PdfCsvGeneralMethod
+  include ItemTypesHelper
   before_action :set_item_type, only: [:show, :edit, :update, :destroy, :get_item_type_products]
 
   # GET /item_types
   # GET /item_types.json
   def index
     @q = ItemType.ransack(params[:q])
-    if @q.result.count > 0
-      @q.sorts = 'id asc' if @q.sorts.empty?
-    end
-    if params[:q].present?
-      @title = params[:q][:title_eq]
-      @code = params[:q][:code_eq]
-    end
+    @q.sorts = 'id asc' if @q.sorts.empty? && @q.result.count.positive?
+    @options_for_select = ItemType.all
     @item_types = @q.result(distinct: true).page(params[:page])
-    respond_to do |format|
-      format.js {
-        @item_types = @q.result(distinct: true)
-        format.js
-      }
-      format.html
-    end
+    download_item_types_csv_file if params[:csv].present?
+    download_item_types_pdf_file if params[:pdf].present?
+    send_email_file if params[:email].present?
+    export_file if params[:export_data].present?
   end
 
   # GET /item_types/1
@@ -93,5 +87,32 @@ class ItemTypesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_type_params
       params.require(:item_type).permit(:title, :code, :comment)
+    end
+    def download_item_type_csv_file
+      @item_type = @q.result
+      header_for_csv = %w[Id Title Comment]
+      data_for_csv = get_data_for_item_type_csv
+      generate_csv(data_for_csv, header_for_csv, 'item_type')
+    end
+  
+    def download_item_type_pdf_file
+      @item_type = @q.result
+      generate_pdf(@item_type.as_json, 'Item_type', 'pdf.html', 'A4')
+    end
+  
+    def send_email_file
+      EmailJob.perform_later(@q.result.as_json, 'item_type/index.pdf.erb', params[:email_value],
+                             params[:email_choice], params[:subject], params[:body],
+                             current_user, 'item_type')
+      if params[:email_value].present?
+        flash[:notice] = "Email has been sent to #{params[:email_value]}"
+      else
+        flash[:notice] = "Email has been sent to #{current_user.email}"
+      end
+      redirect_to cities_path
+    end
+  
+    def export_file
+      export_data('City')
     end
 end
--- app/helpers/item_types_helper.rb
+++ app/helpers/item_types_helper.rb
@@ -1,2 +1,13 @@
 module ItemTypesHelper
+    def get_data_for_cities_csv
+		temp=[]
+		@item_types.each do |item_type|
+			first=item_type.id
+			second=item_type.title
+			third=item_type.comment
+            fourth=item_type
+			temp.push([first, second, third,fourth])
+		end
+		return temp
+	end
 end
