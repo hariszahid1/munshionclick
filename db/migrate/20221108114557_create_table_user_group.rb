48cf341748154a96ab1ab17e85119437c6803733
Salman Tahir <salmantahir3249@gmail.com>
2022-11-08 16:50:27 +0500

    Migration for creating user group table.

---
+++ db/migrate/20221108114557_create_table_user_group.rb
@@ -0,0 +1,10 @@
+class CreateTableUserGroup < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_groups do |t|
+      t.string :title
+      t.text :comment
+
+      t.timestamps
+    end
+  end
+end
