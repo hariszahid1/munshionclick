12812a59c6af07a04861f0396420be8e454bb084
Aysha Areej <aysha@devbox.co>
2022-11-22 11:10:22 +0500

    associated-data-counts

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,7 +1,8 @@
 
 class ProductCategory < ApplicationRecord
   validates_uniqueness_of :code
-
+  has_many :product_sub_categories
+  has_many :products
   has_paper_trail ignore: [:updated_at]
   validates :title, presence: true, uniqueness: { case_sensitive: false }
 
