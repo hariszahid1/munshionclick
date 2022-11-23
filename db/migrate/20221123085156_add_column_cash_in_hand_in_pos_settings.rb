247112a1b4a17996bbaa1359e93e5cbf3a7db89e
Salman Tahir <salmantahir3249@gmail.com>
2022-11-23 18:22:35 +0500

    Repleced data in cash in hand field.

--- db/migrate/20221123085156_add_column_cash_in_hand_in_pos_settings.rb
+++ db/migrate/20221123085156_add_column_cash_in_hand_in_pos_settings.rb
@@ -1,5 +1,6 @@
 class AddColumnCashInHandInPosSettings < ActiveRecord::Migration[6.1]
   def change
     add_column :pos_settings, :cash_in_hand, :decimal
+    PosSetting.update(cash_in_hand: Account.sum(:amount))
   end
 end
