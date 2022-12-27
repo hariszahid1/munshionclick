65f072a34eba3aba16634f84e41cddd824c4d0b3
Hassan Mehmood <hassanmehmood.psr@gmail.com>
2022-12-27 18:47:30 +0500

    CRMS and Orders Trello Tasks Completed

--- app/controllers/crms_controller.rb
+++ app/controllers/crms_controller.rb
@@ -2,7 +2,7 @@
 
 # CRM Controller
 class CrmsController < ApplicationController
-	before_action :check_access
+  before_action :check_access
   before_action :index_search_data
   before_action :set_sys_user, only: %i[show edit update destroy]
   before_action :new_edit_data, only: %i[new edit create index]
@@ -14,7 +14,6 @@ class CrmsController < ApplicationController
   # GET /crms
   # GET /crms.json
   def index
-
     @q = SysUser.order('id desc').ransack(params[:q])
     @sys_users = @q.result.page(params[:page])
     export_file if params[:export_data].present?
@@ -76,20 +75,24 @@ class CrmsController < ApplicationController
 
   def view_history
     @start_date = Date.today.beginning_of_month
-    @end_date =  Date.today.end_of_month
+    @end_date = Date.today.end_of_month
     if params[:q].present?
       @start_date = params[:q][:created_at_gteq] if params[:q][:created_at_gteq].present?
       @end_date = params[:q][:created_at_lteq] if params[:q][:created_at_lteq].present?
       @item_id = params[:q][:item_id_eq] if params[:q][:item_id_eq].present?
-      params[:q][:created_at_lteq] = params[:q][:created_at_lteq].to_date.end_of_day if params[:q][:created_at_lteq].present?
+      if params[:q][:created_at_lteq].present?
+        params[:q][:created_at_lteq] =
+          params[:q][:created_at_lteq].to_date.end_of_day
+      end
     end
     @event = %w[create update destroy]
-    @q = PaperTrail::Version.where(item_type:"SysUser").order('created_at desc').ransack(params[:q])
+    @q = PaperTrail::Version.where(item_type: 'SysUser').order('created_at desc').ransack(params[:q])
     @cms_user_logs = @q.result.page(params[:page])
     respond_to do |format|
       format.js
     end
   end
+
   private
 
   def sys_user_params
@@ -113,43 +116,43 @@ class CrmsController < ApplicationController
       :nom_father,
       :nom_cnic,
       :nom_relation,
-      :contact_attributes => [
-        :id,
-        :address,
-        :home,
-        :office,
-        :mobile,
-        :fax,
-        :email,
-        :comment,
-        :status,
-        :city_id,
-        :country_id,
-        :sys_user_id,
-        :permanent_address
+      contact_attributes: %i[
+        id
+        address
+        home
+        office
+        mobile
+        fax
+        email
+        comment
+        status
+        city_id
+        country_id
+        sys_user_id
+        permanent_address
       ],
-      :notes_attributes => [
-        :id,
-        :message,
-        :assigned_to_id,
-        :created_by,
-        :notable_id,
-        :notable_type
+      notes_attributes: %i[
+        id
+        message
+        assigned_to_id
+        created_by
+        notable_id
+        notable_type
       ],
-      :follow_ups_attributes =>[
-        :id,
-        :reminder_type,
-        :task_type,
-        :priority,
-        :assigned_to_id,
-        :created_by,
-        :date,
-        :comment,
-        :followable_id,
-        :followable_type
+      follow_ups_attributes: %i[
+        id
+        reminder_type
+        task_type
+        priority
+        assigned_to_id
+        created_by
+        date
+        comment
+        followable_id
+        followable_type
       ],
       cms_data: {}
-      )
+    )
   end
 
   def set_sys_user
@@ -188,18 +191,18 @@ class CrmsController < ApplicationController
     EmailJob.perform_later(@q.result.as_json, 'crms/index.pdf.erb', params[:email_value],
                            params[:email_choice], params[:subject], params[:body],
                            current_user, 'crms')
-    if params[:email_value].present?
-      flash[:notice] = "Email has been sent to #{params[:email_value]}"
-    else
-      flash[:notice] = "Email has been sent to #{current_user.email}"
-    end
+    flash[:notice] = if params[:email_value].present?
+                       "Email has been sent to #{params[:email_value]}"
+                     else
+                       "Email has been sent to #{current_user.email}"
+                     end
     redirect_to crms_path
   end
 
   def export_file
     @sys_user = SysUser.all
     header_for_csv = %w[id number name agent_id plot_size short_details project_name client_type client_status
-                       category deal_status source]
+                        category deal_status source]
     data_for_csv = get_data_for_crm_export
     generate_csv(data_for_csv, header_for_csv, 'CRM')
   end
@@ -207,7 +210,8 @@ class CrmsController < ApplicationController
   def index_search_data
     @options_for_select = SysUser.all
     created_by_ids = current_user.created_by_ids_list_to_view
-		@all_agents = User.where('company_type=? or created_by_id=?',current_user.company_type,created_by_ids).pluck(:name, :id)
+    @all_agents = User.where('company_type=? or created_by_id=?', current_user.company_type, created_by_ids).pluck(:name,
+                                                                                                                   :id)
     @all_user = SysUser.all
     @all_plot_sizes = SysUser.pluck(:ntn).uniq
     @user_types = UserType.all
@@ -218,25 +222,38 @@ class CrmsController < ApplicationController
 end
 
 def crm_charts
-    
-    @total_agent_count = SysUser.group(:credit_status).count.except(0, nil).sort.to_h
-    agent_ids = @total_agent_count.keys
-    @agent_name = User.where(id:agent_ids).pluck(:name)
-    @agent_count = @total_agent_count.values
-    
-    @client_count = SysUser.group("cms_data->'$.client_type'").count.except(nil, '', "\"\"")
-    @ct_name = @client_count.keys.map { |a| a.gsub(' ', '-') }
-    @ct_type = @client_count.values
+  @total_agent_count = SysUser.group(:credit_status).count.except(0, nil).sort.to_h
+  agent_ids = @total_agent_count.keys
+  @agent_name = User.where(id: agent_ids).pluck(:name).map { |a| a.gsub(' ', '-') }
+  @agent_count = @total_agent_count.values
 
-    @deal_status_count = SysUser.group("cms_data->'$.deal_status'").count.except(nil, '', "\"\"")
-    @deal_status = @deal_status_count.keys.map { |a| a.gsub(' ', '-') }
-    @deal_count = @deal_status_count.values
+  @client_count = SysUser.group("cms_data->'$.client_type'").count.except(nil, '', '""')
+  @ct_name = @client_count.keys.map { |a| a.gsub(' ', '-') }
+  @ct_type = @client_count.values
 
-    @category_count = SysUser.group("cms_data->'$.category'").count.except(nil, '', "\"\"")
-    @ctg_name = @category_count.keys.map { |a| a.gsub(' ', '-') }
-    @ctg_count = @category_count.values
+  @deal_status_count = SysUser.group("cms_data->'$.deal_status'").count.except(nil, '', '""')
+  @deal_status = @deal_status_count.keys.map { |a| a.gsub(' ', '-') }
+  @deal_count = @deal_status_count.values
 
-    @project_count = SysUser.group("cms_data->'$.project_name'").count.except(nil, '', "\"\"")
-    @prg_name = @project_count.keys.map { |a| a.gsub(' ', '-') }
-    @prg_count = @project_count.values
+  @category_count = SysUser.group("cms_data->'$.category'").count.except(nil, '', '""')
+  @ctg_name = @category_count.keys.map { |a| a.gsub(' ', '-') }
+  @ctg_count = @category_count.values
+
+  @project_count = SysUser.group("cms_data->'$.project_name'").count.except(nil, '', '""')
+  @prg_name = @project_count.keys.map { |a| a.gsub(' ', '-') }
+  @prg_count = @project_count.values
+
+  @in_process_count = 0
+  @closed_count = 0
+  @dead_count = 0
+  @deal_status_count.each do |a|
+    if a.first.include?('dead')
+      @dead_count = a[1]
+    elsif a.first.include?('closed')
+      @closed_count = a[1]
+    elsif a.first.include?('process')
+      @in_process_count = a[1]
+    end
+  end
+  
 end
