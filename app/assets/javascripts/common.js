8ea09a179f6637bcbfa91de72d04724b0a01401f
MuhammadTayyab960 <muhammad.tayyab@devbox.co>
2022-12-28 20:43:39 +0500

    add carriage staff and loading in inwards and iwards pdfs

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -687,6 +687,22 @@ function carriageCostUpdate(){
   // costUpdateReturn();
 }
 
+function inwardCarriageCostUpdate(){
+  var carriage = $("#inward_purchase_sale_detail_carriage_value").val()!= "" ? parseFloat($("#inward_purchase_sale_detail_carriage_value").val()) : 0
+  var loading = $("#inward_purchase_sale_detail_loading_value").val()!= "" ? parseFloat($("#inward_purchase_sale_detail_loading_value").val()) : 0
+  var total_stock = $("#order_inward_total").text()!= "" ? parseFloat($("#order_inward_total").text()) : 0
+  $("#inward_purchase_sale_detail_carriage").val((carriage*total_stock))
+  $("#inward_purchase_sale_detail_loading").val((loading*total_stock))
+}
+
+function inwardCarriageUpdate(){
+  var carriage = $("#inward_purchase_sale_detail_carriage").val()!= "" ? parseFloat($("#inward_purchase_sale_detail_carriage").val()) : 0
+  var loading = $("#inward_purchase_sale_detail_loading").val()!= "" ? parseFloat($("#inward_purchase_sale_detail_loading").val()) : 0
+  var total_stock = $("#order_inward_total").text()!= "" ? parseFloat($("#order_inward_total").text()) : 0
+  $("#inward_purchase_sale_detail_carriage_value").val((carriage/total_stock).toFixed(2))
+  $("#inward_purchase_sale_detail_loading_value").val((loading/total_stock).toFixed(2))
+}
+
 function carriageCostUpdateHscheme(){
   var i;
   var quantity=0;
--- app/helpers/inwards_helper.rb
+++ app/helpers/inwards_helper.rb
@@ -7,6 +7,7 @@ module InwardsHelper
     @sorted_data = []
     @pdf_orders.each do |o|
       @sorted_data << {
+		                id: o.id, 
                         type: o.transaction_type,
 												name: o.sys_user.name,
 												status: o.status,
@@ -22,5 +23,25 @@ module InwardsHelper
 												}
                        }
     end
+  end
+
+  def sorted_show_data
+    @sorted_data = []
+      @sorted_data << {
+		                id: @order.id,
+                        type: @order.transaction_type,
+												name: @order.sys_user.name,
+												status: @order.status,
+												amount: @order.amount,
+												items: @order.order_items.map { |i|
+												{ product: i.product.title,
+													marka: i.marka,
+													builty_no: i.builty_no,
+													vehicle_no: i.vehicle_no,
+													challan_no: i.challan_no,
+													quantity: i.quantity
+												}
+												}
+                       }
   end
 end
