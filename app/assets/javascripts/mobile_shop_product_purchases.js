2c6a695382f7d463ea6560e65b0dcda3d11029cb
MuhammadTayyab960 <muhammad.tayyab@devbox.co>
2023-01-31 20:33:12 +0500

    set index view for mobile shop purchase sale index and add serial and product quantity check

--- app/assets/javascripts/mobile_shop_product_purchases.js
+++ app/assets/javascripts/mobile_shop_product_purchases.js
@@ -108,10 +108,19 @@ function mobile_serail_number_return(value){
   }
   function save_mobile_sale()
   {
-    var serial_fields = $('.serial_no');
-    for(var i = 0; i < serial_fields.length; i++){
-      var value = $.trim($(serial_fields[i]).val());
-      $(serial_fields[i]).val(value);
+    if($("#pos_setting_sys_type").val()=="MobileShop"){
+      var serial_fields = $('.serial_no');
+      var quantity_fields = $('.quantity-price')
+      for(var i = 0; i < serial_fields.length; i++){
+        var value = $.trim($(serial_fields[i]).val());
+        var q_field = parseInt($(quantity_fields[i]).val())
+        var s_field = $(serial_fields[i]).val().split('\n').filter(e => String(e).trim()).length
+        if(q_field != s_field)
+        {
+          window.alert("Please Match the Quantity and Serial Number!")
+          return false;
+        }
+      }
     }
     return_type=true;
     if($("#pos_setting_sys_type").val()!="HousingScheme")
