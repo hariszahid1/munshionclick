eab2afb634501288bb5f00c6a34b6df2c3dd375b
Salman Tahir <salmantahir3249@gmail.com>
2023-02-09 11:31:17 +0500

    Handles nil for crms and sale deals.

--- app/controllers/crms_controller.rb
+++ app/controllers/crms_controller.rb
@@ -189,12 +189,12 @@ class CrmsController < ApplicationController
     @user_groups = UserGroup.all
     @staff = Staff.all
     @pos_setting = PosSetting.last
-    @project_name = @pos_setting.extra_settings.present? ? PosSetting.last.extra_settings['project_name']&.map(&:downcase) : []
-    @client_type = @pos_setting.extra_settings.present? ? @pos_setting.extra_settings['client_type']&.map(&:downcase) : []
-    @client_status = @pos_setting.extra_settings.present? ? @pos_setting.extra_settings['client_status'] : []
-    @category = @pos_setting.extra_settings.present? ? @pos_setting.extra_settings['category']&.map(&:downcase) : []
-    @deal_stat = @pos_setting.extra_settings.present? ? @pos_setting.extra_settings['deal_status']&.map(&:downcase) : []
-    @source = @pos_setting.extra_settings.present? ? @pos_setting.extra_settings['source']&.map(&:downcase) : []
+    @project_name = get_setting('project_name')
+    @client_type = get_setting('client_type')
+    @client_status = get_setting('client_status')
+    @category = get_setting('category')
+    @deal_stat = get_setting('deal_status')
+    @source = get_setting('source')
     created_by_ids = current_user.created_by_ids_list_to_view
     roles_mask = current_user.allowed_to_view_roles_mask_for
     @users = User.where(roles_mask: roles_mask).where('company_type=? or created_by_id=?', current_user.company_type,
@@ -245,6 +245,10 @@ class CrmsController < ApplicationController
     @user_groups = UserGroup.all
     @total_followups = FollowUp.where(followable_type: 'SysUser').group(:followable_id).count
     @follow_up_count = FollowUp.where(created_at: Time.current.all_day).count
+  end
+
+  def get_setting(setting_name)
+    @pos_setting&.extra_settings&.dig(setting_name)&.map(&:downcase) || []
   end
 end
 
